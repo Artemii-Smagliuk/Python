9d5beb415c14a370fd554ef1a745824486dc0eae
smaglart <smaglart@mail.ru>
2023-07-13 17:53:00 +0300

    Finally finished

--- Seminar7/Task36/Task36.py
+++ Seminar7/Task36/Task36.py
@@ -5,3 +5,31 @@
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, 
 # как, например, у операции умножения. 
 
+import ast
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    err = False
+    for y in range(1, num_rows + 1):
+        for x in range(1, num_columns + 1):
+            try:
+                print(f'{operation(x, y)} \t', end='')
+            except TypeError:
+                print('Ошибка данных, введена не функция')
+                err = True
+                break
+        if err:
+            break
+        print('\n')
+
+
+flag = True
+while flag:
+    operation = input("Введите функцию в формате lambda x, y: x * y ")
+    try:
+        operation = eval(operation)
+        flag = False
+    except ValueError or SyntaxError or TypeError or NameError:
+        print(f'Введён некорректный формат функции, повторите ввод')
+
+print_operation_table(operation)
